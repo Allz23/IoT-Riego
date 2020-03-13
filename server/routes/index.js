e5a08a0ff0213d46ff09789c7282b2b8f08b1f8e
Chady Suleiman <Proy-CSuleiman@DRONENA.COM>
2020-03-13 16:30:35 -0400

    Configuracion del enrutador

---
+++ server/routes/index.js
@@ -0,0 +1,7 @@
+// Rutas principales de la API
+const { Router } = require("express");
+
+const router = Router();
+
+// Exportamos las rutas ----------------------------------------
+module.exports = router;
