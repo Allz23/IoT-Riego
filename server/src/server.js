57940f0ea86a1b6bc1cee774edf26e31a1ed2314
Josseline Perez Pasante <Proy-JPerez@DRONENA.com>
2020-03-16 16:44:58 -0400

    Añadido almacenamiento de sesiones en MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -2,11 +2,15 @@
 const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const expSessions = require("express-session");
+const mongoStore = require("connect-mongo")(expSessions);
 
 // - Configuraciones
 const app = express();
+require("../config/database");
 
 const PORT = process.env.PORT || 80;
 app.use(express.static(__dirname + "/public/"));
@@ -18,13 +22,20 @@ app.use(morgan("dev"));
 app.use(
   expSessions({
     secret: "secretApp",
-    saveUninitialized: true,
-    resave: true
+    saveUninitialized: false,
+    resave: false,
+    // Configuracion para almacenar la sesion en la base de datos
+    store: new mongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 180 * 60 * 1000 }
   })
 );
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Rutas
+app.use(require("../routes/index"));
 
 // Iniciamos el servidor
 app.listen(app.get("port"), () => {
