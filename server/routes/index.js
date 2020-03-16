9309c582993fd5f69a3be34170dafb19cb4c4766
Josseline Perez Pasante <Proy-JPerez@DRONENA.com>
2020-03-16 16:44:26 -0400

    Creacion de rutas basicas

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,5 +3,14 @@ const { Router } = require("express");
 
 const router = Router();
 
+router.get("/", (req, res) => {
+  res.json({ mensaje: "Bienvenido al sistema." });
+});
+
+// Ruta de redirecionamiento para errores 404
+router.get("*", (req, res) => {
+  res.json({ mensaje: "Ha ocurrido un problema con el servidor." });
+});
+
 // Exportamos las rutas ----------------------------------------
 module.exports = router;
