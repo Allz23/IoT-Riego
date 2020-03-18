683226fa9c429a999a0666d984dfa222e2176aa2
Josseline Perez Pasante <Proy-JPerez@DRONENA.com>
2020-03-18 13:28:55 -0400

    Modificaciones al archivo

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Listado de electrovalvulas y su estado actual</h1>
+    <b-button class="mt-3">Consulta MongoDB</b-button>
   </div>
 </template>
