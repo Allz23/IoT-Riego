6e2db54e04880e4ffdecd006b9024a7de4f95534
Josseline Perez Pasante <Proy-JPerez@DRONENA.com>
2020-03-25 11:22:03 -0400

    Cambios en el archivo

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   chainWebpack: config => {
     config.plugin("html").tap(args => {
-      args[0].title = "IoT Riego";
-      args[0].meta = {
-        viewport: "width=device-width, initial-scale=1.0"
-      };
-
+      args[0].title = "Your new title";
       return args;
     });
   }
