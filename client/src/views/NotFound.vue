26e718222112c0a9fd6f9bc6b9ec1a83d8d556f2
Josseline Perez Pasante <Proy-JPerez@DRONENA.com>
2020-03-27 10:19:03 -0400

    Añadida funcionalidad de la pagina para error 404

---
+++ client/src/views/NotFound.vue
@@ -0,0 +1,33 @@
+<template>
+  <main class="not-found">
+    <img
+      alt="404"
+      src="../assets/img/404 - UFO.svg"
+      width="400"
+      height="400"
+      class="my-3"
+    />
+    <h1>Página no encontrada.</h1>
+    <p class="more-info">
+      Lo sentimos, pero no encontramos la página que está buscando.<br />
+      Quizás fue movida a otra dirección (¡o secuestrada!).<br />
+      Revise la dirección o haga clic debajo para volver a la página principal.
+    </p>
+    <div class="actions">
+      <router-link
+        tag="a"
+        :to="{ name: 'Home' }"
+        class="btn btn-outline-success"
+        >Página principal</router-link
+      >
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  title: "IoT Riego | Error 404"
+};
+</script>
+
+<style></style>
