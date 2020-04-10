bc6ad7c543d8b0b5bcb7dc750a6d1740d63c510e
Jacinto Acosta <jacintoac24@gmail.com>
2020-04-10 14:56:02 -0400

    Agregadas nuevas rutas y guardias de navegacion

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+// Importacion de vistas
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
 import NotFound from "../views/NotFound.vue";
+// Importacion de componentes
+
+// Importaciones varias
+// import estadoUsuario from "../store/modules/usuarios/state";
 
 Vue.use(VueRouter);
 
@@ -21,6 +27,12 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
   {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: { visitante: true }
+  },
+  {
     path: "*",
     component: NotFound
   }
@@ -31,5 +43,26 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 });
+
+// Guardias de navegacion en las rutas
+// router.beforeEach((to, from, next) => {
+//   // Es recomendado usar este metodo para verificar las rutas
+//   if (to.matched.some(r => r.meta.privada) && !estadoUsuario.usuario) {
+//     next({
+//       name: "Login",
+//       // Usado para redirigir al usuario a su ruta deseada
+//       params: {
+//         wantedRoute: to.fullPath
+//       }
+//     });
+//     return;
+//   }
+
+//   if (to.matched.some(r => r.meta.visitante) && estadoUsuario.usuario) {
+//     next({ name: "Home" });
+//     return;
+//   }
+//   next();
+// });
 
 export default router;
