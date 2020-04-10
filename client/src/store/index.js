eed8effc9cc55b238520bcc911cd6a136187db02
Jacinto Acosta <jacintoac24@gmail.com>
2020-04-10 14:57:44 -0400

    Añadidas tiendas de Vuex para los modulos "Usuarios" y "Valvulas"

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+// Modulos de la tienda
+import usuarios from "./modules/usuarios/index";
+import valvulas from "./modules/valvulas/index";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: {
+    usuarios,
+    valvulas
+  }
 });
