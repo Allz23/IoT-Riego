c760b5937f30ff28fb291e3d88ecbf86a496d133
Jacinto Acosta <jacintoac24@gmail.com>
2020-04-10 14:58:25 -0400

    Cambios en la organizacion y añadidos metodos de Vuex

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -15,19 +15,33 @@
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item :to="{ path: '/' }" exact>Principal</b-nav-item>
-          <b-nav-item :to="{ path: '/about' }" exact>Acerca De</b-nav-item>
+          <b-nav-item :to="{ path: '/about' }" exact v-if="sesion"
+            >Válvulas</b-nav-item
+          >
         </b-navbar-nav>
+        <!-- TODO: Mostrar ciertos items cuando el usuario inicie sesión -->
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right>
+          <b-nav-item-dropdown right v-if="sesion">
             <!-- Using 'button-content' slot -->
             <template v-slot:button-content>
-              <em>Usuario</em>
+              <em>{{ usuario.nombre }}</em>
             </template>
-            <b-dropdown-item href="#">Configuración</b-dropdown-item>
-            <b-dropdown-item href="#">Salir</b-dropdown-item>
+            <b-dropdown-item href="#">
+              <b-icon-gear></b-icon-gear>
+              Configuración
+            </b-dropdown-item>
+            <b-dropdown-item @click="cerrarSesion">
+              <!-- <b-icon-x-circle></b-icon-x-circle> -->
+              <b-icon icon="power"></b-icon>
+              Salir
+            </b-dropdown-item>
           </b-nav-item-dropdown>
+
+          <b-nav-item :to="{ path: '/login' }" exact v-else
+            >Inicio de sesión</b-nav-item
+          >
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -35,18 +49,32 @@
 </template>
 
 <script>
-export default {};
+  import { createNamespacedHelpers } from "vuex";
+  // Importamos las acciones del módulo "usuarios"
+  const {
+    mapState: usuarioState,
+    mapActions: usuarioActions
+  } = createNamespacedHelpers("usuarios");
+
+  export default {
+    computed: {
+      ...usuarioState(["usuario", "sesion"])
+    },
+    methods: {
+      ...usuarioActions(["cerrarSesion"])
+    }
+  };
 </script>
 
 <style lang="scss">
-#navBar ul.navbar-nav > li.nav-item > a.nav-link {
-  border-bottom: 3px solid transparent;
-}
+  // #navBar ul.navbar-nav > li.nav-item > a.nav-link {
+  //   border-bottom: 3px solid transparent;
+  // }
 
-#navBar ul.navbar-nav > li.nav-item > a.nav-link:hover {
-  border-bottom: 3px solid white;
-}
-#navBar ul.navbar-nav > li.nav-item > a.nav-link.router-link-active {
-  border-bottom: 3px solid white;
-}
+  // #navBar ul.navbar-nav > li.nav-item > a.nav-link:hover {
+  //   border-bottom: 3px solid white;
+  // }
+  // #navBar ul.navbar-nav > li.nav-item > a.nav-link.router-link-active {
+  //   border-bottom: 3px solid white;
+  // }
 </style>
