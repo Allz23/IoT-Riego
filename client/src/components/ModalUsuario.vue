fe66487b0844206517590ab13e76922b011bd54d
Jacinto Acosta <jacintoac24@gmail.com>
2020-04-10 15:00:50 -0400

    Añadido modal donde ira la info del usuario

---
+++ client/src/components/ModalUsuario.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <b-modal id="modalUsuario" size="lg" title="Información personal."
+      >Modal para la información del usuario.</b-modal
+    >
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style></style>
