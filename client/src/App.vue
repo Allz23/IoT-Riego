8200175708563f04086437809997944febace430
Jacinto Acosta <jacintoac24@gmail.com>
2020-04-10 14:53:13 -0400

    Añadido soporte para transiciones

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,41 @@
 <template>
   <div id="app">
     <NavBar />
-    <router-view />
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
 <script>
-import NavBar from "./components/NavBar";
-export default {
-  components: {
-    NavBar
-  }
-};
+  import NavBar from "./components/NavBar";
+  export default {
+    components: {
+      NavBar
+    }
+  };
 </script>
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css?family=Montserrat&display=swap");
-#app {
-  font-family: "Montserrat", Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+  @import "./assets/css/Animate.css";
+  @import url("https://fonts.googleapis.com/css?family=Montserrat&display=swap");
+  #app {
+    font-family: "Montserrat", Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  #nav {
+    padding: 30px;
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
-}
 </style>
