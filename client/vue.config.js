55f538513be45d2c8a62d0382b334663343ce646
Jacinto Acosta <jacintoac24@gmail.com>
2020-06-03 10:38:04 -0400

    Modificaciones minimas en archivo

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,29 @@
 module.exports = {
   chainWebpack: config => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap(options => {
+        // Nos permitirá usar direcciones relativas para las imagenes en <b-img></b-img>
+        options.transformAssetUrls = {
+          img: "src",
+          image: "xlink:href",
+          "b-avatar": "src",
+          "b-img": "src",
+          "b-img-lazy": ["src", "blank-src"],
+          "b-card": "img-src",
+          "b-card-img": "src",
+          "b-card-img-lazy": ["src", "blank-src"],
+          "b-carousel-slide": "img-src",
+          "b-embed": "src"
+        };
+
+        return options;
+      });
+
     config.plugin("html").tap(args => {
-      args[0].title = "Your new title";
+      args[0].title = "IoT Nena";
       return args;
     });
   }
