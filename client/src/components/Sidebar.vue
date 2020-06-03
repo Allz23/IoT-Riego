768babe891c7578eb32f51b07495a318903ad3da
Jacinto Acosta <jacintoac24@gmail.com>
2020-06-03 10:38:46 -0400

    Añadida sidebar para navegacion entre modulos

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-sidebar id="sidebar" title="IoT Nena" shadow>
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+        <img
+          src="../assets/img/IoT - Riego (Prueba).svg"
+          height="50"
+          width="50"
+          alt=""
+        />
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style></style>
